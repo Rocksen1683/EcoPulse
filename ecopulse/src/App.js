926d6a56dc962210846e71d3cd81bd6d8a8d9f50
mehtaViraj <mehta.viraj.03@gmail.com>
2024-01-10 22:37:49 -0500

    fixes

--- ecopulse/src/App.js
+++ ecopulse/src/App.js
@@ -43,36 +43,36 @@ export default function Example() {
             />
           </div>
   
-              <div className="mx-auto max-w-2xl text-center">
-                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  EcoPulse
-                </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                  By: Cyclic Geese
-                </p>
-              </div>
+          <div className="mx-auto max-w-2xl text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              EcoPulse
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              By: Cyclic Geese
+            </p>
+          </div>
    
         </div>
-        <div>
-      <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-        Enter your API key
-      </label>
-      <div className="mt-2">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder="you@example.com"
-        />
-      </div>
-      <button
-        className="mt-2 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        onClick={handleEnter}
-      >
-        Enter
-      </button>
-    </div>
+        <div className='px-6'>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Enter your API key
+          </label>
+          <div className="mt-2">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="you@example.com"
+            />
+          </div>
+          <button
+            className="mt-2 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={handleEnter}
+          >
+            Enter
+          </button>
+        </div>
       </main>
 
       {/* Footer */}

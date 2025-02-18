efdc3ba35c22d79178c0aa28f89e60c3060f8b80
Rocksen1683 <edpravee@uwaterloo.ca>
2025-02-17 19:10:04 -0800

    updated deployment

--- ecopulse/src/App.js
+++ ecopulse/src/App.js
@@ -50,7 +50,7 @@ export default function Example() {
     formData.append('apiKey', apiKey);
     setIsBaselineLoading(true);
     try{
-    const response = await fetch('https://f2a9-174-88-242-62.ngrok-free.app//api/predict', {
+    const response = await fetch('https://ecopulse-k6tr.onrender.com/predict', {
       method: 'POST',
       cache: 'no-cache',
       body: formData,
@@ -89,7 +89,7 @@ console.log(res);
     Object.keys(bodyobj).forEach(key => formData.append(key, bodyobj[key]));
     
     setIsUserModelLoading(true);
-    const res = await fetch('https://f2a9-174-88-242-62.ngrok-free.app//api/user-predict', {
+    const res = await fetch('https://ecopulse-k6tr.onrender.com/user-predict', {
       method: 'POST',
       cache: 'no-cache',
       contentType: 'application/json',
@@ -181,7 +181,7 @@ console.log(res);
               <p className="text-sm text-gray-500">
                 Baseline model loaded! Download your file{' '}
                 <a
-                  href={`https://f2a9-174-88-242-62.ngrok-free.app//api/download/${baseServerFile}`}
+                  href={`https://ecopulse-k6tr.onrender.com/download/${baseServerFile}`}
                   className="text-indigo-600 hover:text-purple-800"
                 >
                   here
@@ -605,7 +605,7 @@ console.log(res);
               <p className="text-sm text-gray-500">
                 User model loaded! Download your file{' '}
                 <a
-                  href={`https://f2a9-174-88-242-62.ngrok-free.app//api/download/${userServerFile}`}
+                  href={`https://ecopulse-k6tr.onrender.com/download/${userServerFile}`}
                   className="text-indigo-600 hover:text-purple-800"
                 >
                   here
@@ -621,7 +621,7 @@ console.log(res);
                 <p className="text-sm text-gray-500">
                   Filtered model data loaded! Download your file{' '}
                   <a
-                    href={`https://f2a9-174-88-242-62.ngrok-free.app//api/download/${filteredServerFile}`}
+                    href={`https://ecopulse-k6tr.onrender.com/download/${filteredServerFile}`}
                     className="text-indigo-600 hover:text-purple-800"
                   >
                     here
